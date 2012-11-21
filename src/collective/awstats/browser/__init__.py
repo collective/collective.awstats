7e7156379562a87cc47c213f802b4414601d1c38
Robert Niederreiter <office@squarewave.at>
2012-11-21 19:24:34 +0100

    object stats wip

--- src/collective/awstats/browser/__init__.py
+++ src/collective/awstats/browser/__init__.py
@@ -1,1 +1,11 @@
-#
+from Products.Five import BrowserView
+
+
+class StatsTab(BrowserView):
+
+    def display(self):
+        try:
+            field = self.context.getField('awstats_enabled')
+        except AttributeError, e:
+            return False
+        return field.get(self.context)
