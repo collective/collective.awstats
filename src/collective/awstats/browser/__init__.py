da78bbed09a01c3d2da484d1a1df67e3c8ad2cbc
Robert Niederreiter <office@squarewave.at>
2012-11-27 20:37:38 +0100

    more error prone

--- src/collective/awstats/browser/__init__.py
+++ src/collective/awstats/browser/__init__.py
@@ -6,6 +6,6 @@ class StatsTab(BrowserView):
     def __call__(self):
         try:
             field = self.context.getField('awstats_enabled')
+            return field.get(self.context)
         except AttributeError, e:
             return False
-        return field.get(self.context)
